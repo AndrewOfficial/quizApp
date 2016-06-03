12f63ff4eb7dcc9cbbebb9f8a08f64a287fe6c6f
Andrew Dourgarian <andrewdourgarian@yahoo.com>
2016-06-02 23:01:47 -0500

    Mainly question adding/removing

--- public/scripts/controllers/makeQuizCtrl.js
+++ public/scripts/controllers/makeQuizCtrl.js
@@ -1,7 +1,23 @@
 app.controller('makeQuizCtrl', function ($scope, quizFactory){
 
+  $scope.quiz = quizFactory.makeQuizObject.get();
+  $scope.questionQuantity = $scope.quiz.questions.length;
+  console.log($scope.quiz);
   $scope.saveQuiz = function(){
+    console.log($scope.quiz)
+  };
 
+  $scope.changeQuestionQuantity = function (){
+    if ($scope.questionQuantity > $scope.quiz.questions.length){
+      for (var i = $scope.quiz.questions.length; i < $scope.questionQuantity; i++){
+        quizFactory.addQuestion(i);
+      }
+    } else {
+      for (var i = $scope.quiz.questions.length; i > $scope.questionQuantity; i--){
+        quizFactory.removeQuestion(i);
+      }
+    }
+    $scope.quiz = quizFactory.makeQuizObject.get();
   }
 
 });
--- public/scripts/factories/quizFactory.js
+++ public/scripts/factories/quizFactory.js
@@ -3,27 +3,39 @@ app.factory('quizFactory',[function(){
   var quizTemplate = {
     title:'',
     questions: [
-    ]
+    ],
+    questionsRemoved: []
   };
 
   quizFactory.makeQuizObject = {
     set: function (title, questions){
       quizTemplate.title = title;
       console.log(title, questions);
-      for (var i in questions){
-        quizTemplate.questions.push({
-          question: '',
-          answer:0,
-          options: [
-
-          ]
-        })
+      for (var i = 0; i < questions; i++){
+        quizFactory.addQuestion(i);
       }
     },
     get: function (){
-
+      return quizTemplate;
     }
   };
 
+  quizFactory.addQuestion = function(i){
+    quizTemplate.questions.push({
+      number: i,
+      inQuery: '',
+      options: [
+        {
+          answer: '',
+          bool: false
+        }
+      ]
+    })
+  };
+
+  quizFactory.removeQuestion = function(i){
+    var x = quizTemplate.questions.pop();
+    quizTemplate.questionsRemoved.push(x);
+  };
   return quizFactory;
 }]);
